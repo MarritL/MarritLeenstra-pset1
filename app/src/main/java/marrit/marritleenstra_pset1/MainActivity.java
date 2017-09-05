159c10e045d87030c016e71d5c0c09dc13305122
Marrit <marritleenstra@hotmail.com>
2017-09-05 18:31:19 +0200

    Initial commit

---
+++ app/src/main/java/marrit/marritleenstra_pset1/MainActivity.java
@@ -0,0 +1,13 @@
+package marrit.marritleenstra_pset1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
