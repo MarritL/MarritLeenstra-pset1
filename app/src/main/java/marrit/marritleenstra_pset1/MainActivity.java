9adb85ce94048092b40fab101be0defd706e0d5b
Marrit <marritleenstra@hotmail.com>
2017-09-08 18:42:36 +0200

    listener for eyes working

--- app/src/main/java/marrit/marritleenstra_pset1/MainActivity.java
+++ app/src/main/java/marrit/marritleenstra_pset1/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.CheckBox;
 import android.widget.ImageView;
 
 
-
+// declare a seperate class for all bodyparts
 class bodypart {
 
     // Properties of the class
@@ -30,49 +30,73 @@ class bodypart {
 
 }
 
+// main activity
 public class MainActivity extends AppCompatActivity {
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bodypart arms = new bodypart((ImageView) findViewById(R.id.arms));
-        bodypart ears = new bodypart((ImageView) findViewById(R.id.ears));
-        bodypart eyebrows = new bodypart((ImageView) findViewById(R.id.eyebrows));
-        bodypart eyes = new bodypart((ImageView) findViewById(R.id.eyes));
-        bodypart glasses = new bodypart((ImageView) findViewById(R.id.glasses));
-        bodypart hat = new bodypart((ImageView) findViewById(R.id.hat));
-        bodypart mouth = new bodypart((ImageView) findViewById(R.id.Mouth));
-        bodypart mustache = new bodypart((ImageView) findViewById(R.id.mustache));
-        bodypart nose = new bodypart((ImageView) findViewById(R.id.nose));
-        bodypart shoes = new bodypart((ImageView) findViewById(R.id.shoes));
+        // construct all bodyparts onCreate
+        final bodypart arms = new bodypart((ImageView) findViewById(R.id.arms));
+        final bodypart ears = new bodypart((ImageView) findViewById(R.id.ears));
+        final bodypart eyebrows = new bodypart((ImageView) findViewById(R.id.eyebrows));
+        final bodypart eyes = new bodypart((ImageView) findViewById(R.id.eyes));
+        final bodypart glasses = new bodypart((ImageView) findViewById(R.id.glasses));
+        final bodypart hat = new bodypart((ImageView) findViewById(R.id.hat));
+        final bodypart mouth = new bodypart((ImageView) findViewById(R.id.Mouth));
+        final bodypart mustache = new bodypart((ImageView) findViewById(R.id.mustache));
+        final bodypart nose = new bodypart((ImageView) findViewById(R.id.nose));
+        final bodypart shoes = new bodypart((ImageView) findViewById(R.id.shoes));
 
+        // construct all checkboxes onCreate
         final CheckBox CBArms = (CheckBox) findViewById(R.id.checkBoxArms) ;
-        CheckBox CBEars = (CheckBox) findViewById(R.id.checkBoxEars);
-        CheckBox CBEyebrows = (CheckBox) findViewById(R.id.checkboxEyebrows);
+        final CheckBox CBEars = (CheckBox) findViewById(R.id.checkBoxEars);
+        final CheckBox CBEyebrows = (CheckBox) findViewById(R.id.checkboxEyebrows);
         final CheckBox CBEyes = (CheckBox) findViewById(R.id.checkBoxEyes);
-        CheckBox CBGlasses = (CheckBox) findViewById(R.id.checkBoxGlasses);
+        final CheckBox CBGlasses = (CheckBox) findViewById(R.id.checkBoxGlasses);
 
+        // hide all bodyparts when app is started (onCreate)
         arms.hide();
         eyes.hide();
+        ears.hide();
+        eyebrows.hide();
+        glasses.hide();
+        hat.hide();
+        mouth.hide();
+        mustache.hide();
+        nose.hide();
+        shoes.hide();
 
-        CBEyes.setChecked(true);
+        // set listener for clicks on checkboxes
+        // method learned from: http://tekeye.uk/android/examples/code-android-event-listeners
+        CBEyes.setOnClickListener(new View.OnClickListener() {
 
-        if (CBArms.isChecked()) {
-            arms.show();
-        }
-        if (CBEyes.isChecked()) {
-            eyes.show();
-        }
+            public void onClick(View v) {
+
+                if (CBEyes.isChecked())
+                    eyes.show();
+                else
+                    eyes.hide();
+            }
+        });
+
+        //CBEyes.setChecked(true);
+
+       // if (CBArms.isChecked()) {
+       //     arms.show();
+       // }
+       // if (CBEyes.isChecked()) {
+       //     eyes.show();
+       // }
+
+    }
 
 
 
 
-}}
+}
 
 
 /*
