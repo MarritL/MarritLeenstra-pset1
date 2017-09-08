feb3e4c9cc864942071f2d376ae6fa050d36d7d4
Marrit <marritleenstra@hotmail.com>
2017-09-08 18:08:23 +0200

    body class working with methods show/hide. Next has to be implemented the onclick-listeners (don't work yet).

--- app/src/main/java/marrit/marritleenstra_pset1/MainActivity.java
+++ app/src/main/java/marrit/marritleenstra_pset1/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 
+
+
 class bodypart {
 
     // Properties of the class
@@ -21,62 +23,58 @@ class bodypart {
     public void show() {
         part.setVisibility(View.VISIBLE);
     }
+
     public void hide() {
         part.setVisibility(View.INVISIBLE);
     }
+
 }
 
 public class MainActivity extends AppCompatActivity {
 
-    bodypart arms = new bodypart((ImageView) findViewById(R.id.arms));
-    bodypart ears = new bodypart((ImageView) findViewById(R.id.ears));
-    bodypart eyebrows = new bodypart((ImageView) findViewById(R.id.eyebrows));
-    bodypart eyes = new bodypart((ImageView) findViewById(R.id.eyes));
-    bodypart glasses = new bodypart((ImageView) findViewById(R.id.glasses));
-    bodypart hat = new bodypart((ImageView) findViewById(R.id.hat));
-    bodypart mouth = new bodypart((ImageView) findViewById(R.id.Mouth));
-    bodypart mustache = new bodypart((ImageView) findViewById(R.id.mustache));
-    bodypart nose = new bodypart((ImageView) findViewById(R.id.nose));
-    bodypart shoes = new bodypart((ImageView)findViewById(R.id.shoes));
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // source: https://developer.android.com/guide/topics/ui/controls/checkbox.html
+        bodypart arms = new bodypart((ImageView) findViewById(R.id.arms));
+        bodypart ears = new bodypart((ImageView) findViewById(R.id.ears));
+        bodypart eyebrows = new bodypart((ImageView) findViewById(R.id.eyebrows));
+        bodypart eyes = new bodypart((ImageView) findViewById(R.id.eyes));
+        bodypart glasses = new bodypart((ImageView) findViewById(R.id.glasses));
+        bodypart hat = new bodypart((ImageView) findViewById(R.id.hat));
+        bodypart mouth = new bodypart((ImageView) findViewById(R.id.Mouth));
+        bodypart mustache = new bodypart((ImageView) findViewById(R.id.mustache));
+        bodypart nose = new bodypart((ImageView) findViewById(R.id.nose));
+        bodypart shoes = new bodypart((ImageView) findViewById(R.id.shoes));
 
-    public void clicked(View view) {
-        // Is the view now checked?
-        boolean checked = ((CheckBox) view).isChecked();
+        final CheckBox CBArms = (CheckBox) findViewById(R.id.checkBoxArms) ;
+        CheckBox CBEars = (CheckBox) findViewById(R.id.checkBoxEars);
+        CheckBox CBEyebrows = (CheckBox) findViewById(R.id.checkboxEyebrows);
+        final CheckBox CBEyes = (CheckBox) findViewById(R.id.checkBoxEyes);
+        CheckBox CBGlasses = (CheckBox) findViewById(R.id.checkBoxGlasses);
 
-        // Check which checkbox was clicked
-        switch (view.getId()) {
-            case R.id.checkBoxEyes:
-                if (checked)
-                    eyes.show();
-                else
-                    eyes.hide();
-                break;
-            case R.id.checkboxEyebrows:
-                if (checked)
-                    // make eyebrows visible
-                    eyebrows.show();
-                else
-                    eyebrows.hide();
-                break;
-            case R.id.checkBoxGlasses:
-                if (checked)
-                    glasses.show();
-                else
-                    glasses.hide();
-                break;
+        arms.hide();
+        eyes.hide();
+
+        CBEyes.setChecked(true);
+
+        if (CBArms.isChecked()) {
+            arms.show();
         }
-    }
+        if (CBEyes.isChecked()) {
+            eyes.show();
+        }
 
 
-}
-}
+
+
+}}
+
+
 /*
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
